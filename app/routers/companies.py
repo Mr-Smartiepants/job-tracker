ca35e06260af6f17e67139ad4dd17e4cdff3943e
Mr.Smartiepants <st.stefanius@googlemail.com>
2026-03-16 14:03:11 +0100

    Implemented full CRUD for Company resource (POST, GET, PATCH, DELETE)

--- app/routers/companies.py
+++ app/routers/companies.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from app.database.connection import SessionLocal
 from app.models.company import Company
 from app.schemas.company import CompanyCreate
+from app.schemas.company import CompanyUpdate
 
 router = APIRouter()
 
@@ -20,6 +21,73 @@ def create_company(company: CompanyCreate):
         db.refresh(db_company)
 
         return db_company
+
+    finally:
+        db.close()
+
+@router.get("/")
+def get_companies():
+    db = SessionLocal()
+
+    try:
+        companies = db.query(Company).all()
+        return companies
+    finally:
+        db.close()
+
+@router.get("/{company_id}")
+def get_company(company_id: int):
+    db = SessionLocal()
+
+    try:
+        company = db.query(Company).filter(Company.id == company_id).first()
+
+        if company is None:
+            return {"error": "Company not found"}
+
+        return company
+
+    finally:
+        db.close()
+
+@router.delete("/{company_id}")
+def delete_company(company_id: int):
+    db = SessionLocal()
+
+    try:
+        company = db.query(Company).filter(Company.id == company_id).first()
+
+        if company is None:
+            return {"error": "Company not found"}
+
+        db.delete(company)
+        db.commit()
+
+        return {"message": "Company deleted"}
+
+    finally:
+        db.close()
+
+@router.patch("/{company_id}")
+def update_company(company_id: int, company_update: CompanyUpdate):
+    db = SessionLocal()
+
+    try:
+        company = db.query(Company).filter(Company.id == company_id).first()
+
+        if company is None:
+            return {"error": "Company not found"}
+
+        if company_update.name is not None:
+            company.name = company_update.name
+
+        if company_update.location is not None:
+            company.location = company_update.location
+
+        db.commit()
+        db.refresh(company)
+
+        return company
 
     finally:
         db.close()
